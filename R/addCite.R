82ffca8552aa7c04d2e528961399a31a3802ffce
Pieta Schofield <pieta.schofield@me.com>
2015-07-02 17:40:37 +0100

    changes to addCite to make more general

--- R/addCite.R
+++ R/addCite.R
@@ -3,9 +3,16 @@
 #' @param packages
 #'
 #' @export
-addCite <- function(bib,packages,type="T"){
-  switch(type,
-    "T"=TextCite(bib,listcitekeys(packages=packages)),
-    "A"=AutoCite(bib,listcitekeys(packages=packages))
-  )
+addCite <- function(packages,type="T",bib=NULL){
+  if(!is.null(bib)){
+    switch(type,
+      "T"=TextCite(bib,listcitekeys(packages=packages)),
+      "A"=AutoCite(bib,listcitekeys(packages=packages))
+    )
+  }else{
+    switch(type,
+      "T"= paste0("\\citet{",paste(listcitekeys(packages),collapse=","),"}"),
+      "A"= paste0("\\citep{",paste(listcitekeys(packages),collapse=","),"}")
+    )
+  }
 }

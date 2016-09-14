a0aa5fa863828b6d455225e7748942d32ff73d05
Pieta Schofield <pschofield@login01.(none)>
2016-09-14 10:09:51 +0100

    First CRUK based commit
    beginning of changes required for job submission to CRUK cluster and
    other structural reorganisation

---
+++ R/rcur.R
@@ -0,0 +1,14 @@
+#' render the current document  
+#'
+#' @param pname project name
+#' @param fname file name
+#' @param tname tree name
+#' @param rname root of tree
+#' @param oflag open a file
+#'
+#' @export
+rcur <- function(fileName=.curFile,projDir=.curProj,inDir="Code",outDir="Projects"){
+  homeDir <- "/home/pschofield"
+  rmarkdown::render(file.path(homeDir,inDir,projDir,paste0(fileName,".Rmd")),
+                    output_dir=file.path(homeDir,outDir,projDir))
+}

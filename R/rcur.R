7107451056183be200ced6c94b7f8f544eaf1bc6
Pieta Schofield <pieta.schofield@me.com>
2016-09-15 15:14:06 +0100

    CRUK submission system changes

--- R/rcur.R
+++ R/rcur.R
@@ -7,7 +7,7 @@
 #' @param oflag open a file
 #'
 #' @export
-rcur <- function(fileName=.curFile,projDir=.curProj,inDir="Code",outDir="Projects"){
+rcur <- function(fileName=.curFile,projDir=.curProj,inDir="Code",outDir="Notes/Projects"){
   homeDir <- "/home/pschofield"
   rmarkdown::render(file.path(homeDir,inDir,projDir,paste0(fileName,".Rmd")),
                     output_dir=file.path(homeDir,outDir,projDir))

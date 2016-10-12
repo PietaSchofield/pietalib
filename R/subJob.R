fdde568b336e14a22f8406fb88512c37fa9878ed
Pieta Schofield <pieta.schofield@me.com>
2016-10-12 18:23:34 +0100

    clean out some stuff that has moved to cruk

--- R/subJob.R
+++ R/subJob.R
@@ -5,15 +5,12 @@
 #' @param batchFile a string representing the name of the script file to be submitted
 #' @param user a string with the user name of the person making the ssh connection
 #' @param host a string with the name of the login host for the cluster
-#' @param logDir a string with the location of where the job logs files will be stored
-#' @param queue queue name
-#' @param mcCores an integer for the number of cores to request
-#' @param email job status email setting
-#' @param emailAddress where to send job status emails
+#' @param qsubstring the submission string for the torque system
 #' @param args any arguments to add to the job submission string
+#' @param noSub unless set to false will merely display the command rather than submit it.
 #'
 #' @export
-subJob <- function(scriptfile,user="pschofield", host="troodon.scicom.picr.man.ac.uk",
+subJobCRUK <- function(scriptfile,user="pschofield", host="troodon.scicom.picr.man.ac.uk",
                    qsubString="qsub",args=NULL, noSub=T )
 {
   jobString <- paste0("'source /etc/bashrc;", qsubString," ",scriptfile, " ",args," ")
